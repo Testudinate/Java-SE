d41ff4a0250e0f9f48454502dd8af5a3d07e1e95
Developer <klevtsovstanislav@gmial.com>
2015-11-08 23:56:08 +0300

    Create 03_hello_world.java

---
+++ 03_hello_world.java
@@ -0,0 +1,14 @@
+Напишите программу, выводящую в консоль Hello world!
+Класс должен называться "Main", это ограничение проверяющей системы.
+
+Sample Input:
+
+Sample Output:
+
+It's alive! It's alive!
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
