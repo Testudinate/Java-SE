f605c19d60cbb980c4f571933ddc47ef1af3671e
Developer <klevtsovstanislav@gmial.com>
2016-06-27 18:01:59 +0300

    Create 1.java

---
+++ JavaRush/1.java
@@ -0,0 +1,45 @@
+Создайте объект типа Cat(кот), объект типа Dog (собака), объект типа Fish (рыбка) и объект типа Woman.
+Присвойте каждому животному владельца (owner).
+
+
+package com.javarush.test.level02.lesson05.task03;
+
+/* У каждого животного должна быть хозяйка.
+Создайте объект типа Cat(кот), объект типа Dog (собака), объект типа Fish (рыбка) и объект типа Woman.
+Присвойте каждому животному владельца (owner).
+*/
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        //напишите тут ваш код
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+        Fish fish = new Fish();
+        Woman woman = new Woman();
+        
+                dog.owner=woman;
+               fish.owner=woman;
+                cat.owner=woman;
+        
+    }
+
+    public static class Cat
+    {
+        public Woman owner;
+    }
+
+    public static class Dog
+    {
+        public Woman owner;
+    }
+
+    public static class Fish
+    {
+        public Woman owner;
+    }
+
+    public static class Woman
+    {
+    }
+}
